c9b48a1845018aecbd6ba8461d8e5ad03a530c46
salidamaharjan <salidamaharjan@outlook.com>
2024-04-20 12:36:48 -0500

    Optimise the code

--- Algorithms/L0002StringPalindrome.java
+++ Algorithms/L0002StringPalindrome.java
@@ -10,17 +10,12 @@ public class L0002StringPalindrome {
         String lowerCase = str.toLowerCase();
         char[] charArray = lowerCase.toCharArray();
         double mid = Math.floor(charArray.length / 2);
-        int comparing = charArray.length - 1;
-        boolean palindrome = true;
-        System.out.println(mid);
+        // System.out.println(mid);
         for (int i = 0; i < mid; i++) {
-            if (charArray[i] == charArray[comparing]) {
-                comparing -= 1;
-                palindrome = true;
-            } else {
-                return palindrome = false;
+            if (charArray[i] != charArray[charArray.length - 1 - i]) {
+                return false;
             }
         }
-        return palindrome;
+        return true;
     }
 }
