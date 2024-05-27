51388474189c7dfbf4cb091a964c8140fe52c593
salidamaharjan <salidamaharjan@outlook.com>
2024-05-27 12:29:18 -0500

    Setup of Java app with maven and create gitignore file

---
+++ java-with-maven/src/main/java/dev/smhr/App.java
@@ -0,0 +1,13 @@
+package dev.smhr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
