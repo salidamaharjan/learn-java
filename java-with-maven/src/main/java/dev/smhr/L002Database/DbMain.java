69c3b6f0ce24d628f6f0e676a4656aadd900fb7d
salidamaharjan <salidamaharjan@outlook.com>
2024-05-29 22:33:03 -0500

    Create table in DB, insert value and do CRUD operation using SQL

--- java-with-maven/src/main/java/dev/smhr/L002Database/DbMain.java
+++ java-with-maven/src/main/java/dev/smhr/L002Database/DbMain.java
@@ -1,7 +1,48 @@
 package dev.smhr.L002Database;
 
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.Connection;
+
 public class DbMain {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello World");
+        String connectionDbURL = "jdbc:postgresql://localhost:5432/learn_java_db";
+        String dbUsername = "root";
+        String dbPassword = "password";
+
+        // Step1: Register a driver
+        Class.forName("org.postgresql.Driver");
+        // Step2: acquire a connection
+        Connection conn = DriverManager.getConnection(connectionDbURL, dbUsername, dbPassword);
+        // Step3: create statement
+        Statement statement = conn.createStatement();
+        // Data Definition Language (DDL)
+        statement.execute("DROP TABLE IF EXISTS test_students");
+        statement.execute("""
+                CREATE TABLE IF NOT EXISTS test_students (
+                    id SERIAL PRIMARY KEY,
+                    full_name VARCHAR(100) NOT NULL,
+                    age INT NOT NULL,
+                    address VARCHAR(100)
+                )
+                """);
+        // Data Modification Language (DML)
+        statement.executeUpdate("""
+                INSERT INTO test_students (full_name, age, address)
+                VALUES ('Salida', 31, 'PriorLake'),
+                ('John', 33, 'Bloomington'),
+                ('Jane', 33, 'Lakeville')
+                """);
+        statement.executeUpdate("""
+                UPDATE test_students
+                SET full_name = 'ABC', age = 40, address='XYZ'
+                WHERE id = 3
+                """);
+        statement.executeUpdate("""
+                DELETE FROM test_students WHERE id = 2
+                """);
+        // last Step: close connection
+        conn.close();
     }
 }
