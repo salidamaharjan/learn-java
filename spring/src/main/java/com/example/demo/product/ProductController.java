d1377be89ed17f12c86c4938e59ca7bf2cc5d030
salidamaharjan <salidamaharjan@outlook.com>
2024-09-08 17:33:35 -0500

    create db and connect with postgress

--- spring/src/main/java/com/example/demo/product/ProductController.java
+++ spring/src/main/java/com/example/demo/product/ProductController.java
@@ -4,22 +4,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.sql.Connection;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController{
     @GetMapping
-    public ArrayList<Product> getProducts(){
-        ArrayList<Product> product = new ArrayList<>();
-        Product lotion = new Product();
-        lotion.setId(1L);
-        lotion.setTitle("Lotion");
-        product.add(lotion);
-        Product faceWash = new Product();
-        faceWash.setId(2L);
-        faceWash.setTitle("Face Wash");
-        product.add(faceWash);
-        return product;
+    public ArrayList<Product> getProducts() throws Exception {
+        ArrayList<Product> products = new ArrayList<>();
+        //Step 0: optional
+        Class.forName("org.postgresql.Driver");
+        //Step 1: Establish a connection
+        //Required: DB_SERVER_URL, DB_USERNAME, DB_PASSWORD
+        //"<PROTCOL>://<URL>:<PORT>/<DB_NAME>"
+        String dbUrl = "jdbc:postgresql://localhost:5432/smhr_spring_db";
+        String db_username = "root";
+        String db_password = "password";
+
+        Connection conn = DriverManager.getConnection(dbUrl, db_username, db_password);
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM products");
+
+        while(rs.next()){
+            Product p = new Product();
+            p.setId(rs.getLong("id"));
+            p.setTitle(rs.getString("title"));
+            products.add(p);
+        }
+        return products;
     }
 }
